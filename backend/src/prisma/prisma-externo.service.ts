9281701094e23835bfb6139380746330b936753f
ValerioFerreira <valerioalbuquerque@outlook.com>
2026-05-12 13:38:05 -0300

    Serverless

--- backend/src/prisma/prisma-externo.service.ts
+++ backend/src/prisma/prisma-externo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma-externo';
+import { PrismaClient } from '.prisma/client-externo';
 
 @Injectable()
 export class PrismaExternoService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
